40f3e6facbf710918be47891e5fd8b615c6826f5
SuperSuave <21092088+SuperSuave@users.noreply.github.com>
2026-09-03 08:22:29 -0700

    feat: add ble.h header file for Bluetooth Low Energy interface definitions

--- main/ble.h
+++ main/ble.h
@@ -21,10 +21,17 @@
 #ifndef __BLE_H__
 #define __BLE_H__
 
-void ble_init(QueueHandle_t *xTXp_Queue, QueueHandle_t *xRXp_Queue, uint8_t connected_led, int passkey, uint8_t* uid);
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+void ble_init(QueueHandle_t *xTXp_Queue, QueueHandle_t *xRXp_Queue,
+              uint8_t connected_led, int passkey, uint8_t *uid);
 bool ble_connected(void);
-void ble_send(uint8_t* buf, uint8_t buf_len);
+void ble_send(uint8_t *buf, uint8_t buf_len);
 bool ble_tx_ready(void);
 void ble_disable(void);
 void ble_enable(void);
+
 #endif

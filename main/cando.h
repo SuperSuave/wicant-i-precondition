dc3aa0ed3013b40dbba1b449d9f5498b2204da9d
SuperSuave <21092088+SuperSuave@users.noreply.github.com>
2026-09-07 23:54:33 -0700

    fix: so inclusive

--- main/cando.h
+++ main/cando.h
@@ -5,11 +5,13 @@
 
 #include "cJSON.h"
 #include "driver/twai.h"
+#include "esp_driver_twai.h"
 #include "esp_err.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/semphr.h"
 #include <stdbool.h>
 #include <stdint.h>
+
 
 #ifdef __cplusplus
 extern "C" {

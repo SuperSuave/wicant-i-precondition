41c78b0e8045e380d9b990149be76489923153e4
SuperSuave <21092088+SuperSuave@users.noreply.github.com>
2026-09-12 19:58:54 -0700

    chore: update catalog repo location

--- main/ui_src/js/can_do/catalog.js
+++ main/ui_src/js/can_do/catalog.js
@@ -568,7 +568,7 @@ function getCommandTaxonomy(cmd) {
     return { domain: "system_automation", subdomain: "network_integrations" };
 }
 
-const DEFAULT_CAN_DO_CATALOG_URL = "https://raw.githubusercontent.com/SuperSuave/wicant-i-precondition/look-revamp/main/can_do_catalog.json";
+const DEFAULT_CAN_DO_CATALOG_URL = "https://github.com/SuperSuave/CAN-Do-Message-Catalog/blob/main/can_do_catalog.json";
 
 function getCanDoCatalogUrl() {
     let url = localStorage.getItem("wican_can_do_catalog_url") || DEFAULT_CAN_DO_CATALOG_URL;
